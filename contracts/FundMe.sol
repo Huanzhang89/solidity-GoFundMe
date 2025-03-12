affc1a4f33942f14dcba14f657c23f9623747494
Huan Zhang <huanzhang30@gmail.com>
2025-03-12 22:46:44 +0800

    fund  method, price converter and  price data from chainlink

---
+++ contracts/FundMe.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import  {PriceConverter}  from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() > minimumUsd, "didn't send enough Eth");  // 1e18 =  1 ETH = 1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+}
