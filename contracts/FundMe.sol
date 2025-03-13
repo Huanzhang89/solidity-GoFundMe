aebba935365dc7917238ba7a361f272dcd994d1b
Huan Zhang <huanzhang30@gmail.com>
2025-03-13 19:40:40 +0800

    add withdraw function and onlyOwner modifier

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,10 +10,36 @@ contract FundMe {
     uint256 public minimumUsd = 5e18;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+    
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() > minimumUsd, "didn't send enough Eth");  // 1e18 =  1 ETH = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset array
+        funders = new address[](0);
+        // actually withdraw the funds
+        // payable(msg.sender).transfer(address(this).balance);
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;  // this allows us to use the code in the modifier without having to add a parameter
     }
 }
